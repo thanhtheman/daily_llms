3d35b32846c5d0bc19e06e8c2ee7d666d8f1797b
thanhtheman <thanhb.quach@gmail.com>
2023-11-09 21:20:04 -0500

    first commit

---
+++ langchain/concepts/testing.py
@@ -0,0 +1,8 @@
+import requests, os
+from dotenv import load_dotenv
+load_dotenv()
+
+a = [1,2,3]
+
+
+    
---
+++ testing.py
@@ -0,0 +1,18 @@
+a = ["jen", "lub", "kocham", "cie"]
+b = ["john", "lub", "lang"]
+# response = [(index**2, name) for index, name in enumerate(a)]
+# print(response)
+# my_list = iter(a)
+# b = next(my_list)
+# print(b)
+# b = next(my_list)
+# print(b)
+# b = next(my_list)
+# print(b)
+# b = next(my_list)
+# print(b)
+c = "this is a string"
+docs = []
+for i in c:
+    docs.extend(c)
+print(docs)
